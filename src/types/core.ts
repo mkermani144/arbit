6ce31ac85c7e26aeef87c5bdc1cacf58e803e9d6
Mohammad Kermani <mkermani144@gmail.com>
2024-12-03 19:28:14 +0330

    refactor: update chore types

--- src/types/core.ts
+++ src/types/core.ts
@@ -1,40 +1,52 @@
 /**
- * A market (the class itself) is anything the allows swapping pairs, e.g.
- * Uniswap
+ * A provider is anything that have markets for swapping pairs (e.g. Uniswap is
+ * a provider and its LPs are the markets)
  */
-export abstract class Market {
-  constructor(protected id: string) {}
-  abstract x2y(amount: bigint): bigint;
-  abstract y2x(amount: bigint): bigint;
+export interface Provider {
+  x2y(marketId: string, amount: bigint): bigint;
+  y2x(marketId: string, amount: bigint): bigint;
 }
 
 /**
  * Mapping type of a market to the next one in an arbitrategy chain:
  * - xx: token X of this market is token X of the next market
  * - xy: token X of this market is token Y of the next market
+ * - yx: token Y of this market is token X of the next market
+ * - yy: token Y of this market is token Y of the next market
  * - null: used for the last market, indicating chain tail
+ *
+ * @example
+ * For this chain:
+ * --ETH/BTC->ETH/USDT->USDT/USDC->BTC/USDC--
+ * We have these mappings:
+ * --xx->yx->yy->null--
  */
-export type Mapping = "xx" | "xy" | null;
+export type Mapping = "xx" | "xy" | "yx" | "yy" | null;
 
 /**
- * A link is a piece in the arbitrategy chain, containing market id (e.g. LP id)
- * and its mapping type
+ * A link is a piece in the arbitrategy chain, containing provider, market id
+ * (e.g. LP id) and its mapping type
+ *
+ * @example
+ * const link: Link = {
+ *   providerId: 'uniswap',
+ *   marketId: 'awesomeLP',
+ *   mapping: 'xx',
+ * }
  */
 export interface Link {
+  providerId: string;
+  marketId: string;
   mapping: Mapping;
-  id: string;
 }
 
 /**
- * An arbitrategy chain is a complete set of markets that we manipulate to earn
- * profit
+ * An arbitrategy chain is a complete set of markets (possibly on different
+ * providers) that we manipulate to earn profit
  */
 export type ArbitrategyChain = Link[];
 
 /**
  * Arbitrategy indicates a set of chains that we are going to manipulate
  */
-export interface Arbitrategy {
-  providersChain: (typeof Market)[];
-  chains: ArbitrategyChain[];
-}
+export type Arbitrategy = ArbitrategyChain[];
