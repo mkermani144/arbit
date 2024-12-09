2e42b93336fe9a5b9017a65edc9a79fe0c125ca3
Seyed Mojtaba Mirsoleimani <seyedmojtabamirsoleimani@gmail.com>
2024-12-09 10:25:48 -0800

    fixed comments

--- src/providers/splash.ts
+++ src/providers/splash.ts
@@ -2,6 +2,30 @@ import { AmountWithDecimal, Provider } from "@/types/core";
 import { AmmPool, makeNativePools, Pools } from "@ergolabs/ergo-dex-sdk";
 import { AssetAmount, Explorer, RustModule } from "@ergolabs/ergo-sdk";
 
+type Order = {
+  price: string;
+  avgPrice: string;
+  ordersLiquidity: string;
+  poolsLiquidity: string;
+  accumulatedLiquidity: string;
+  volumeRelation: string;
+};
+
+type Pair = {
+  base: string;
+  quote: string;
+};
+
+type OrderBookResponse = {
+  pair: Pair;
+  asks: Order[];
+  bids: Order[];
+  spot: string;
+  previousSpot: string;
+  ammTotalLiquidityBase: string;
+  ammTotalLiquidityQuote: string;
+};
+
 class Splash implements Provider {
   private baseUrl: string;
 
@@ -9,22 +33,18 @@ class Splash implements Provider {
     this.baseUrl = url;
   }
 
-  async getOrderBook(marketId: string) {
-    try {
-      const response = await fetch(
-        `${this.baseUrl}/platform-api/v1/trading-view/order-book?base=${marketId}&quote=.`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      return response.json();
-    } catch (error) {
-      console.error("Error fetching order book:", error);
-      throw new Error("Failed to fetch order book");
-    }
+  async getOrderBook(marketId: string): Promise<OrderBookResponse> {
+    const response = await fetch(
+      `${this.baseUrl}/platform-api/v1/trading-view/order-book?base=${marketId}&quote=.`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    return data as OrderBookResponse;
   }
 
   async x2y(marketId: string, amount: AmountWithDecimal) {
@@ -32,21 +52,17 @@ class Splash implements Provider {
 
     const ask = orderBook.asks[0].price;
 
-    return {
-      amount: ask.toFixed(amount.decimals),
-      decimals: amount.decimals,
-    };
+    return;
+    amount: Number(ask);
   }
 
   async y2x(marketId: string, amount: AmountWithDecimal) {
     const orderBook = await this.getOrderBook(marketId);
 
-    const spot = orderBook.bids[0].price;
+    const bid = orderBook.bids[0].price;
 
-    return {
-      amount: spot.toFixed(amount.decimals),
-      decimals: amount.decimals,
-    };
+    return;
+    amount: Number;
   }
 }
 
