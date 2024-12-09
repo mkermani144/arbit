3eecea32c90c8a78441b032da570ebb0995ac2b9
Mohammad Kermani <mkermani144@gmail.com>
2024-12-09 14:14:59 +0330

    refactor: extract common logic in utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,31 +1,27 @@
 import { PrimaryAsset } from "./types/core";
 
-export const asset2usd = async (asset: PrimaryAsset, amount: number) => {
+const fetchPrice = async (coingeckoId: string) => {
   const response: {
     [key: string]: {
       usd: number;
     };
   } = await (
     await fetch(
-      `https://api.coingecko.com/api/v3/simple/price?ids=${asset.coingeckoId}&vs_currencies=usd`
+      `https://api.coingecko.com/api/v3/simple/price?ids=${coingeckoId}&vs_currencies=usd`
     )
   ).json();
-  const assetValue = response[asset.coingeckoId].usd;
+
+  return response[coingeckoId].usd;
+};
+
+export const asset2usd = async (asset: PrimaryAsset, amount: number) => {
+  const assetValue = await fetchPrice(asset.coingeckoId);
 
   return (assetValue * amount) / asset.decimals;
 };
 
 export const usd2asset = async (asset: PrimaryAsset, usd: number) => {
-  const response: {
-    [key: string]: {
-      usd: number;
-    };
-  } = await (
-    await fetch(
-      `https://api.coingecko.com/api/v3/simple/price?ids=${asset.coingeckoId}&vs_currencies=usd`
-    )
-  ).json();
-  const assetValue = response[asset.coingeckoId].usd;
+  const assetValue = await fetchPrice(asset.coingeckoId);
 
   return (usd * asset.decimals) / assetValue;
 };
