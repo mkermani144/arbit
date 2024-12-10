73d6c2d653d52945c68f3435ae98179867e5b0dc
Mohammad Kermani <mkermani144@gmail.com>
2024-12-10 14:15:46 +0330

    feat: cap precision for numbers

--- src/components/hero-price.tsx
+++ src/components/hero-price.tsx
@@ -6,10 +6,10 @@ interface HeroPriceProps {
 const HeroPrice = ({ usd, percent }: HeroPriceProps) => (
   <>
     <h1 className="scroll-m-20 text-7xl font-extrabold tracking-tight lg:text-9xl">
-      ${usd}
+      ${+usd.toPrecision(6)}
     </h1>
     <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-      {percent}%
+      {+percent.toPrecision(4)}%
     </h2>
   </>
 );
