f6663f97b9311e5fd26f191dcd325b677498b7ab
Mohammad Kermani <mkermani144@gmail.com>
2024-12-10 15:02:01 +0330

    feat: add "alpha" alongside Arbit name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,18 @@ import AppLoading from "./AppLoading";
 
 import GithubRepo from "@/components/github-repo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Badge } from "@/components/ui/badge";
 
 export default async function Home() {
   return (
     <div className="flex h-screen w-full items-center justify-center flex-col px-4">
       <div className="fixed top-4 left-4">
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-          Arbit
-        </h3>
+        <div className="flex gap-2 items-center">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            Arbit
+          </h3>
+          <Badge variant="outline">alpha</Badge>
+        </div>
       </div>
       <div className="fixed top-4 right-4">
         <div className="flex gap-2">
