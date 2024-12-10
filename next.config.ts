47b1593c175d692fe7bb8945081abfafdce2deb6
Mohammad Kermani <mkermani144@gmail.com>
2024-12-10 17:11:27 +0330

    fix: fix next build issue

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { access, symlink } from 'fs/promises';
 import type { NextConfig } from 'next';
+import { join } from 'path';
 
 const nextConfig: NextConfig = {
-  webpack: function (config) {
+  webpack: function (config, { isServer }) {
     config.experiments = {
       asyncWebAssembly: true,
       syncWebAssembly: true,
       layers: true,
     };
+
+    config.plugins.push(
+      new (class {
+        apply(compiler: any) {
+          compiler.hooks.afterEmit.tapPromise(
+            'SymlinkWebpackPlugin',
+            async (compiler: any) => {
+              if (isServer) {
+                const from = join(compiler.options.output.path, '../static');
+                const to = join(compiler.options.output.path, 'static');
+
+                try {
+                  await access(from);
+                  console.log(`${from} already exists`);
+                  return;
+                } catch (error: any) {
+                  if (error.code === 'ENOENT') {
+                    // No link exists
+                  } else {
+                    throw error;
+                  }
+                }
+
+                await symlink(to, from, 'junction');
+                console.log(`created symlink ${from} -> ${to}`);
+              }
+            },
+          );
+        }
+      })(),
+    );
+
     return config;
   },
 };
