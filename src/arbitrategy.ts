f3ebd0df8ffbeb79739a30b604ec0cadec15c56e
Mohammad Kermani <mkermani144@gmail.com>
2025-01-09 12:39:08 +0330

    chore: add Minswap arbits

--- src/arbitrategy.ts
+++ src/arbitrategy.ts
@@ -35,7 +35,74 @@ const adaErgArbitrategy: Arbitrategy = [
       },
     },
   ],
-
+  [
+    {
+      providerId: 'ergodex',
+      swapType: 'y2x',
+      marketId:
+        'ae97c5eccd59a065cd973a8d6afb8bb79f9cc70368a7dcdf73aaeab1cedf6f6b',
+      x: {
+        name: 'ERG',
+        coingeckoId: 'ergo',
+        decimals: 9,
+      },
+      y: {
+        name: 'rsADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+    },
+    {
+      providerId: 'minswap',
+      swapType: 'y2x',
+      marketId:
+        'dc06f64060dfa4119c56f8f5b8c69f897a335e3e9802456f4ae26ff7f6f1b570-f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c-bae40b377b196400f601fb1d04bd75486cfc2b5cbab9329bb49a1dfe93180fdf',
+      x: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+      y: {
+        name: 'rsERG',
+        coingeckoId: 'ergo',
+        decimals: 9,
+      },
+    },
+  ],
+  [
+    {
+      providerId: 'minswap',
+      swapType: 'x2y',
+      marketId:
+        'dc06f64060dfa4119c56f8f5b8c69f897a335e3e9802456f4ae26ff7f6f1b570-f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c-bae40b377b196400f601fb1d04bd75486cfc2b5cbab9329bb49a1dfe93180fdf',
+      x: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+      y: {
+        name: 'rsERG',
+        coingeckoId: 'ergo',
+        decimals: 9,
+      },
+    },
+    {
+      providerId: 'splash',
+      swapType: 'x2y',
+      marketId:
+        '04b95368393c821f180deee8229fbd941baaf9bd748ebcdbf7adbb14.7273455247',
+      x: {
+        name: 'rsERG',
+        coingeckoId: 'ergo',
+        decimals: 9,
+      },
+      y: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+    },
+  ],
   [
     {
       providerId: 'splash',
@@ -51,6 +118,74 @@ const adaErgArbitrategy: Arbitrategy = [
         name: 'ADA',
         coingeckoId: 'cardano',
         decimals: 6,
+      },
+    },
+    {
+      providerId: 'minswap',
+      swapType: 'y2x',
+      marketId:
+        'dc06f64060dfa4119c56f8f5b8c69f897a335e3e9802456f4ae26ff7f6f1b570-f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c-bae40b377b196400f601fb1d04bd75486cfc2b5cbab9329bb49a1dfe93180fdf',
+      x: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+      y: {
+        name: 'rsERG',
+        coingeckoId: 'ergo',
+        decimals: 9,
+      },
+    },
+  ],
+  [
+    {
+      providerId: 'splash',
+      swapType: 'y2x',
+      marketId:
+        '04b95368393c821f180deee8229fbd941baaf9bd748ebcdbf7adbb14.7273455247',
+      x: {
+        name: 'rsERG',
+        coingeckoId: 'ergo',
+        decimals: 9,
+      },
+      y: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+    },
+    {
+      providerId: 'ergodex',
+      swapType: 'x2y',
+      marketId:
+        'ae97c5eccd59a065cd973a8d6afb8bb79f9cc70368a7dcdf73aaeab1cedf6f6b',
+      x: {
+        name: 'ERG',
+        coingeckoId: 'ergo',
+        decimals: 9,
+      },
+      y: {
+        name: 'rsADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+    },
+  ],
+  [
+    {
+      providerId: 'minswap',
+      swapType: 'x2y',
+      marketId:
+        'dc06f64060dfa4119c56f8f5b8c69f897a335e3e9802456f4ae26ff7f6f1b570-f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c-bae40b377b196400f601fb1d04bd75486cfc2b5cbab9329bb49a1dfe93180fdf',
+      x: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+      y: {
+        name: 'rsERG',
+        coingeckoId: 'ergo',
+        decimals: 9,
       },
     },
     {
@@ -109,6 +244,40 @@ const adaRsnArbitrategy: Arbitrategy = [
   ],
   [
     {
+      providerId: 'ergodex',
+      swapType: 'y2x',
+      marketId:
+        '29f45df2736a7c7dd790b682fb15429961d7c1ae92e05c72a4f65d53fc0c47d7',
+      x: {
+        name: 'RSN',
+        coingeckoId: 'rosen-bridge',
+        decimals: 3,
+      },
+      y: {
+        name: 'rsADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+    },
+    {
+      providerId: 'minswap',
+      swapType: 'y2x',
+      marketId:
+        'c37fd1213a1003d1cf360cdf0680a61aaca068c5e175e54448931b40ebc80053-f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c-32b2e465e2e93356c1e167f782bcc0ab8ff2e6d6f4ec225919f008e4d573af45',
+      x: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+      y: {
+        name: 'rsRSN',
+        coingeckoId: 'rosen-bridge',
+        decimals: 3,
+      },
+    },
+  ],
+  [
+    {
       providerId: 'splash',
       swapType: 'y2x',
       marketId:
@@ -136,6 +305,108 @@ const adaRsnArbitrategy: Arbitrategy = [
       },
       y: {
         name: 'rsADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+    },
+  ],
+  [
+    {
+      providerId: 'minswap',
+      swapType: 'x2y',
+      marketId:
+        'c37fd1213a1003d1cf360cdf0680a61aaca068c5e175e54448931b40ebc80053-f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c-32b2e465e2e93356c1e167f782bcc0ab8ff2e6d6f4ec225919f008e4d573af45',
+      x: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+      y: {
+        name: 'rsRSN',
+        coingeckoId: 'rosen-bridge',
+        decimals: 3,
+      },
+    },
+    {
+      providerId: 'ergodex',
+      swapType: 'x2y',
+      marketId:
+        '29f45df2736a7c7dd790b682fb15429961d7c1ae92e05c72a4f65d53fc0c47d7',
+      x: {
+        name: 'RSN',
+        coingeckoId: 'rosen-bridge',
+        decimals: 3,
+      },
+      y: {
+        name: 'rsADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+    },
+  ],
+  [
+    {
+      providerId: 'splash',
+      swapType: 'y2x',
+      marketId:
+        '04b95368393c821f180deee8229fbd941baaf9bd748ebcdbf7adbb14.727352534e',
+      x: {
+        name: 'rsRSN',
+        coingeckoId: 'rosen-bridge',
+        decimals: 3,
+      },
+      y: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+    },
+    {
+      providerId: 'minswap',
+      swapType: 'y2x',
+      marketId:
+        'c37fd1213a1003d1cf360cdf0680a61aaca068c5e175e54448931b40ebc80053-f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c-32b2e465e2e93356c1e167f782bcc0ab8ff2e6d6f4ec225919f008e4d573af45',
+      x: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+      y: {
+        name: 'rsRSN',
+        coingeckoId: 'rosen-bridge',
+        decimals: 3,
+      },
+    },
+  ],
+  [
+    {
+      providerId: 'minswap',
+      swapType: 'x2y',
+      marketId:
+        'c37fd1213a1003d1cf360cdf0680a61aaca068c5e175e54448931b40ebc80053-f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c-32b2e465e2e93356c1e167f782bcc0ab8ff2e6d6f4ec225919f008e4d573af45',
+      x: {
+        name: 'ADA',
+        coingeckoId: 'cardano',
+        decimals: 6,
+      },
+      y: {
+        name: 'rsRSN',
+        coingeckoId: 'rosen-bridge',
+        decimals: 3,
+      },
+    },
+    {
+      providerId: 'splash',
+      swapType: 'x2y',
+      marketId:
+        '04b95368393c821f180deee8229fbd941baaf9bd748ebcdbf7adbb14.727352534e',
+      x: {
+        name: 'rsRSN',
+        coingeckoId: 'rosen-bridge',
+        decimals: 3,
+      },
+      y: {
+        name: 'ADA',
         coingeckoId: 'cardano',
         decimals: 6,
       },
